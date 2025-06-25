1f31c0d704525c4fd1cf083b04eff16a63fe4ffd
Caderthewiz <caderthewiz@outlook.com>
2025-06-25 15:09:24 -0500

    10

--- Simple Ray Tracer/color.h
+++ Simple Ray Tracer/color.h
@@ -7,10 +7,21 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+	if (linear_component > 0)
+		return std::sqrt(linear_component);
+
+	return 0;
+}
+
 void write_color(std::ostream& out, color& pixel_color) {
-	double r = pixel_color.x();
-	double g = pixel_color.y();
-	double b = pixel_color.z();
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	r = linear_to_gamma(r);
+	g = linear_to_gamma(g);
+	b = linear_to_gamma(b);
 
 	//Tranlation
 	static const interval intensity(0.000, 0.999);
--- Simple Ray Tracer/math_util.h
+++ Simple Ray Tracer/math_util.h
@@ -18,7 +18,11 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-	return rand() / (RAND_MAX + 1.0);
+	return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+	return min + (max - min) * random_double();
 }
 
 //Common Headers
